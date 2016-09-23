248a0d27c34a3593af28061549668d970580b3e3
Max Tymchii <samback@ukr.net>
2016-09-23 15:29:55 +0300

    Minor fix.

--- NVActivityIndicatorView/NVActivityIndicatorAnimationDelegate.swift
+++ NVActivityIndicatorView/NVActivityIndicatorAnimationDelegate.swift
@@ -9,5 +9,5 @@
 import UIKit
 
 public protocol NVActivityIndicatorAnimationDelegate {
-    func setUpAnimationInLayer(layer: CALayer, size: CGSize, color: UIColor)
+    func setUpAnimation(in layer: CALayer, size: CGSize, color: UIColor)
 }
